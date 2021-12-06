a4a082ce403f3da7fef02a0fc6e995e21874a34e
Lucy-Yxr <xinrongyang@g.harvard.edu>
2021-12-06 11:54:29 -0500

    add vector tests

--- tests/test_forward_mode.py
+++ tests/test_forward_mode.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 
 from GrADim.forward_mode import ForwardMode
@@ -7,6 +8,8 @@ from GrADim.GrADim import Gradim
 # Run using """python -m pytest tests/test_forward_mode.py"""
 class TestForwardMode:
     X = ForwardMode(2)
+    multiple_X = ForwardMode(np.array([1, 2, 3]))
+
     float_equality_threshold = 1e-8
 
     def test_init(self):
@@ -117,44 +120,46 @@ class TestForwardMode:
         Y = Gradim.abs(self.X)
         assert Y.value == 2
         assert Y.derivative == 1
-    '''
+    
     def test_arcsin(self):
+        self.X = ForwardMode(.5)
         Y = Gradim.arcsin(self.X)
-        assert Y.value == np.arcsin(2)
-        assert Y.derivative == 1 / np.sqrt(1 - 2 ** 2)
-
+        assert Y.value == np.arcsin(.5)
+        assert Y.derivative == 1 / np.sqrt(1 - .5 ** 2)
+    '''
     def test_arccosec(self):
         Y = Gradim.arccosec(self.X)
-        assert Y.value == np.arccosec(2)
-        assert Y.derivative == (-1 / np.sqrt(2 **2 - 1)) * 1/np.abs(self.value)
-
+        assert Y.value == np.arcsin(1/2)
+        assert Y.derivative == (-1 / np.sqrt(2 **2 - 1)) * 1/np.abs(2)
+    '''
     def test_arccos(self):
+        self.X = ForwardMode(.5)
         Y = Gradim.arccos(self.X)
-        assert Y.value == np.acsc(2)
-        assert Y.derivative == -1 / np.sqrt(1 - 2 ** 2)
-
+        assert Y.value == np.arccos(.5)
+        assert Y.derivative == -1 / np.sqrt(1 - .5 ** 2)
+    '''
     def test_arcsec(self):
         Y = Gradim.arcsec(self.X)
-        assert Y.value == np.asec(2)
+        assert Y.value == np.arccos(1/2)
         assert Y.derivative == (1/np.sqrt(2**2 - 1)) * 1/np.abs(2)
-
+    '''
     def test_arctan(self):
         Y = Gradim.arctan(self.X)
         assert Y.value == np.arctan(2)
         assert Y.derivative == 1 / (1 + 2 ** 2)
-
+    '''
     def test_arccot(self):
         Y = Gradim.arccot(self.X)
         assert Y.value == np.acot(2)
         assert Y.derivative == -1 / (1 + 2 ** 2)
-    
+    '''
     def test_complex_function(self):
         Y = Gradim.exp(self.X) * Gradim.sin(self.X) + Gradim.tan(self.X)/Gradim.sin(self.X)
         true_value = np.exp(2) * np.sin(2) + np.tan(2)/np.sin(2)
         true_derivative = np.exp(2) * (np.cos(2) + np.sin(2)) + np.sin(2)/(np.cos(2)**2)
         assert np.abs(Y.value - true_value) < self.float_equality_threshold
         assert np.abs(Y.derivative - true_derivative) < self.float_equality_threshold
-    '''
+    
     def test_polynomial(self):
         Y = self.X**3 + self.X - 2
         assert Y.value == 8
@@ -185,3 +190,31 @@ class TestForwardMode:
         Y = -3 * Gradim.log( 7 * Gradim.abs(self.X) )
         assert Y.value == -3* np.log(14)
         assert Y.derivative == -3/2
+
+
+    def test_function_multiple_inputs(self):
+        def function_multiple_inputs(x):
+            return Gradim.cos(x[0]) + Gradim.exp(x[2])*x[1]
+        Y = function_multiple_inputs(self.multiple_X)
+        return Y.derivative
+        assert Y.value == np.cos(1)+np.exp(3)*2
+        assert (Y.derivative == np.array([-np.sin(1), np.exp(3), 2*np.exp(3)])).all()
+
+    def test_function_multiple_outputs(self):
+        @ForwardMode.multiple_outputs
+        def function_multiple_outputs(x):
+            return 3*x, Gradim.sin(x), Gradim.sqrt(x)
+        Y = function_multiple_outputs(self.X)
+        assert (Y.value == np.array([6, np.sin(2),2**.5])).all()
+        assert (Y.derivative == np.array([3, np.cos(2),(.5)*2**(-.5)])).all()
+
+    def test_function_multiple_inputs_and_outputs(self):
+        @ForwardMode.multiple_outputs
+        def function_multiple_inputs_and_outputs(x):
+            return x[0] + 2*x[1] * x[2], x[0] - x[2]
+        Y = function_multiple_inputs_and_outputs(self.multiple_X)
+        assert (Y.value == np.array([13, -2])).all()
+        assert (Y.derivative == np.array([[1,6,4], [1,0,-1]])).all()
+
+if __name__=="__main__":
+	print("")
