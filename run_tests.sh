a0fec3288cf2edc083ffd3f2ab1cadf68cbf6d3f
Lucy-Yxr <xinrongyang@g.harvard.edu>
2021-12-08 06:44:44 -0500

    Edit to test for both modes

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 coverage run -m pytest tests/test_forward_mode.py
+coverage report -m
+
+coverage run -m pytest tests/test_reverse_mode.py
 # run the code coverage
 coverage report -m
