83d6e9a962a5ae3f4850e0cf0453e44ce3950245
Gregoire Baverez <gregoire.baverez@student-cs.fr>
2021-12-08 10:36:00 -0500

    test coverage on reverse

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Run test files
-coverage run -m pytest tests/test_forward_mode.py test_reverse_mode.py
+coverage run -m pytest tests/test_forward_mode.py tests/test_reverse_mode.py
 
 # Run the code coverage on both
 coverage report -m
